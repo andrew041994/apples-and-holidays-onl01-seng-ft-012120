aa0324271e59518f85bb25c7fb8cb6f5d0b7483a
Alehandro Persaud <andrew041994@gmail.com>
2020-01-26 02:06:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |key,value|
     if key == :winter
-      :winter.each {|key,value| value << supply}
+      key.each {|key,value| value << supply}
     end
 
   end
