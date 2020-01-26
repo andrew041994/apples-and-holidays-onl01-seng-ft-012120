9238aa8e179983dfa31fc8fcbacceee0eab40003
Alehandro Persaud <andrew041994@gmail.com>
2020-01-26 03:04:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each {|season,data|puts season}
+holiday_hash.each {|season,data|puts "#{season}:"
 end
 
 def all_holidays_with_bbq(holiday_hash)
