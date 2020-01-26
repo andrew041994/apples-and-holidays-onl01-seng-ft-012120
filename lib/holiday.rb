51828ad946f41f62567f79cfe9a2c0b5e59799d3
Alehandro Persaud <andrew041994@gmail.com>
2020-01-26 02:45:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
 holiday_hash.each do |k,v|
   if k == season
-    [holiday_name] = supply_array
+    k[holiday_name] = supply_array
   end
 end
 
